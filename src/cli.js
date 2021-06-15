8947651890713d2fe277739dec03e6ff9ea34948
Suyash <suyash.krishna9@gmail>
2021-06-15 13:56:11 +0400

    Create files for route

--- src/cli.js
+++ src/cli.js
@@ -2,14 +2,19 @@ const program = require('commander');
 
 const pkg = require('../package.json');
 const { initGenerateComponentCommand } = require('./commands/generateComponent');
+const { initGenerateRouteCommand } = require('./commands/generateRoute');
 const { getCLIConfigFile } = require('./utils/grcConfigUtils');
 
 module.exports = async function cli(args) {
   const cliConfigFile = await getCLIConfigFile();
 
   // Initialize generate component command
-
-  initGenerateComponentCommand(args, cliConfigFile, program);
+  console.log(args);
+  if (args[2] === 'route') {
+    initGenerateRouteCommand(args, cliConfigFile, program);
+  } else {
+    initGenerateComponentCommand(args, cliConfigFile, program);
+  }
 
   program.version(pkg.version);
   program.parse(args);
